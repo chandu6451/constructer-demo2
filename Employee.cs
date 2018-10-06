78073679c844560400bcc32b795d066d93797a1a
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 12:36:57 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo2
+{
+    public class Employee
+    {
+        int id;
+        string name;
+        double salary;
+        static string company;
+
+        static Employee()
+        {
+            company = "Capgemini";
+        }
+
+        public Employee()
+        {
+            id = 0;
+            name = "";
+            salary = 0.0;
+            //company = "";
+        }
+
+        public Employee(int eid, string ename, double sal)
+        {
+            id = eid;
+            name = ename;
+            salary = sal;
+            //company = comp;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            Employee emp1 = new Employee(101, "Robert", 30000.00);
+
+            Console.ReadKey();
+        }
+    }
+}
